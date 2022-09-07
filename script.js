45973d7c69229158a91321590121cbe0712ed674
Aditya Manikonda <adityamanikonda123@gmail.com>
2022-09-07 02:26:04 -0700

    done with pressKey() function, need to debug why keys aren't being displayed

--- script.js
+++ script.js
@@ -15289,10 +15289,21 @@ const dictionary = [
     "rural",
     "shave"
   ]
+const WORD_LENGTH = 5
+const guessGrid = document.querySelector("[data-guess-grid]")
+const offsetFromDate = new Date(2022, 0, 1)
+const targetWord = ""
+
+startInteraction()
 
 function startInteraction() {
     document.addEventListener("click", handleMouseClick)
-    document.addEventListener("keydown", handleKeyClick)
+    document.addEventListener("keydown", handleKeyPress)
+}
+
+function stopInteraction() {
+    document.removeEventListener("click", handleMouseClick)
+    document.removeEventListener("keydown", handleKeyPress)
 }
 
 function handleMouseClick(e) {
@@ -15312,6 +15323,49 @@ function handleMouseClick(e) {
     }
 }
 
-function handleKeyClick(e) {
-    return
+function handleKeyPress(e) {
+    if (e.key === "Enter") {
+        submitGuess()
+        return
+    }
+
+    if (e.key === "Backspace" || e.key === "Delete") {
+        deleteKey()
+        return
+    }
+
+    if (e.key.match(/^[a-z]$/)) {
+        pressKey(e.key)
+        return
+    }
+}
+
+function pressKey(key) {
+    const activeTiles = getActiveTiles()
+    if (activeTiles.length >= WORD_LENGTH) {
+        return
+    }
+    const nextTile = guessGrid.querySelector(":not([data-letter])")
+    nextTile.dataset.letter = key.toLowerCase()
+    nextTile.textContent = key
+    nextTile.dataset.state = "active"
+}
+
+function deleteKey() {
+    const activeTiles = getActiveTiles()
+    const lastTile = activeTiles[activeTiles.length - 1]
+    if (lastTile == null) {
+        return
+    }
+    lastTile.textContent = ""
+    delete lastTile.dataset.state
+    delete lastTile.dataset.letter
+}
+
+function submitGuess() {
+
+}
+
+function getActiveTiles() {
+    return guessGrid.querySelectorAll('[data-state="active"]')
 }
